2bd17cb81f475779d6ff7c283cf94ce616e7a246
MkGodh <fenryrvla@yandex.ru>
2024-01-21 00:48:15 +0300

    Добавлен метод void, для проверки на длинну равную или меньше 3 символов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array1 = new string[8] {"Hey", "321", "Madara", "Uchiha", "777", "22", "Shinobi", "Est"};
 string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}   
